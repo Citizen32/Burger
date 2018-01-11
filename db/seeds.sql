9ccf5b5e3ed018c1db4f15a22d2632bc942228fa
David Morales <morgar.creative@gmail.com>
2018-01-10 18:52:13 -0600

    Created files and folders. Also, mySQL data base was created with some values

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name) VALUE ('Texas Bacon Cheeseburger');
+INSERT INTO burgers(burger_name) VALUE ('Avocado Veggie Burger');
+INSERT INTO burgers(burger_name) VALUE ('Chipotle Mango Burger');
